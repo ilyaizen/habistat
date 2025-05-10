57425716e11a38a6e97fa77ae597df147eb77cc9
Ilya Aizenberg <ilyaizen@gmail.com>
2025-05-10 14:44:52 +0300

    Initial commit

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -10,7 +10,7 @@
         default: "bg-primary text-primary-foreground hover:bg-primary/90 shadow",
         destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm",
         outline:
-          "border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm",
+          "button-outline border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline"
